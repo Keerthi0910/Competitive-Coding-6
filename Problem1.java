0509d6cd370799ef69b64817377624a135b8a84f
Keerthi0910 <chundurikeerthipriya@gmail.com>
2026-09-10 22:52:14 -0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
+// Rate limiter
+//O(n) time complexity
+// O(n) space complexity
 
+class Logger {
+
+	HashMap<String, Integer> map;
+
+    public Logger() {
+
+		this.map = new HashMap<>();
+
+    }
+
+    public boolean shouldPrintMessage(int timestamp, String message) {
+
+		if(map.containsKey(message)  &&  map.get(message) > timeStamp) {
+			return false;
+		}
+
+		map.put(message, timeStamp+10);
+		return true;
+
+    }
+}
